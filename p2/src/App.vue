ba26d69f91d840dc6dc4790d3029406b15ce776f
matthew kim <matthewjkim@gmail.com>
2020-11-17 09:10:54 -0500

    build

--- p2/src/App.vue
+++ p2/src/App.vue
@@ -7,7 +7,7 @@
         />
 
         <p>
-            Record script readings with anyone in the world!
+            Record script readings with anyone in the world! Go to a script and hover over any line. Hold down the microphone button (you'll have to allow microphone settings when asked) and start speaking. Let go of the button and it will save your audio file. Click on each line to play the line! It will be green if there's an audio file available.
         </p>
 
         <nav>
